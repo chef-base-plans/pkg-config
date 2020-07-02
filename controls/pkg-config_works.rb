f40e1c46ebd3b91c26731e58c03a1cc4f26179af
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-07-02 13:13:51 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/pkg-config_works.rb
+++ controls/pkg-config_works.rb
@@ -7,9 +7,9 @@ control 'core-plans-pkg-config-works' do
   impact 1.0
   title 'Ensure pkg-config works as expected'
   desc '
-  Verify pkg-config by ensuring 
-  (1) its installation directory exists and 
-  (2) that it returns the expected version.
+  Verify pkg-config by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -19,9 +19,8 @@ control 'core-plans-pkg-config-works' do
     its('stderr') { should be_empty }
   end
   
-  command_relative_path = input('command_relative_path', value: 'bin/pkg-config')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", "pkg-config")
   describe command("#{command_full_path} --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
